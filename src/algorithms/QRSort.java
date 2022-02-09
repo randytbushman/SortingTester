2e56e3faac34c20bbc463168b75190e89ad7514b
randytbushman <bushmanr1@students.rowan.edu>
2022-02-09 10:47:58 -0500

    Added documentation

--- src/algorithms/QRSort.java
+++ src/algorithms/QRSort.java
@@ -9,7 +9,7 @@ public class QRSort {
         sort(arr, arr.length);
     }
 
-    public static int[] sort(int[] arr, int maxRemainder)
+    public static int[] sort(int[] arr, int divisor)
     {
         int i;
         // Find the minimum and maximum values
@@ -19,33 +19,33 @@ public class QRSort {
                 minValue = arr[i];
             else if(arr[i] > maxValue)
                 maxValue = arr[i];
-        int maxQuotient = ((maxValue - minValue) / maxRemainder) + 1;
 
-        // Shadow array that is meant for temporary storage of all values
+        int maxQuotient = ((maxValue - minValue) / divisor) + 1;
+
+        // Shadow array that is meant for temporary storage for all values
         int[] shadowArr = new int[arr.length];
+        int[] countingArr = new int[Math.max(divisor, maxQuotient)];
 
-        int[] countingArr = new int[Math.max(maxRemainder, maxQuotient)];
-
-        // Perform Counting Sort, sorting based on remainder values
+        // Perform Counting Sort with remainder values
         for(i = 0; i < arr.length; ++i)
-            ++countingArr[(arr[i] - minValue) % maxRemainder];
-        for(i = 1; i < maxRemainder; ++i)
+            ++countingArr[(arr[i] - minValue) % divisor];
+        for(i = 1; i < divisor; ++i)
             countingArr[i] += countingArr[i-1];
         for(i = arr.length-1; i > -1; --i)
-            shadowArr[--countingArr[(arr[i] - minValue) % maxRemainder]] = arr[i];
+            shadowArr[--countingArr[(arr[i] - minValue) % divisor]] = arr[i];
 
         if(maxQuotient > 1) {
-            // Reset the values in order to reuse this counting array for sorting quotients
-            for(i = 0; i < maxRemainder; ++i)
+            // Reset the values to reuse counting array for sorting quotients
+            for(i = 0; i < divisor; ++i)
                 countingArr[i] = 0;
 
-            // Perform Counting Sort, sorting based on quotient values
+            // Perform Counting Sort on quotient values
             for(i = 0; i < arr.length; ++i)
-                ++countingArr[(shadowArr[i] - minValue) / maxRemainder];
+                ++countingArr[(shadowArr[i] - minValue) / divisor];
             for(i = 1; i < countingArr.length; ++i)
                 countingArr[i] += countingArr[i-1];
             for(i = arr.length-1; i > -1; --i)
-                arr[--countingArr[(shadowArr[i] - minValue) / maxRemainder]] = shadowArr[i];
+                arr[--countingArr[(shadowArr[i] - minValue) / divisor]] = shadowArr[i];
         }
         else
             for(i = 0; i < arr.length; ++i)
